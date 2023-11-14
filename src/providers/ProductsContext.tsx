c055478d5608bfaa8b2449811958759a4fc7c120
gstv <gustavo.bmc76@gmail.com>
2023-11-14 17:10:04 -0300

    feat: format

--- src/providers/ProductsContext.tsx
+++ src/providers/ProductsContext.tsx
@@ -1,23 +1,26 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { api } from "@/services/api";
 import { IProductContext, IProductShop } from "./@types";
 
 export const ProductsContext = createContext({} as IProductContext);
 
-export const ProductsProvider = ({ children }: { children: React.ReactNode }) => {
-
+export const ProductsProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   const [shopList, setShopList] = useState<IProductShop[]>([]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { isLoading, data:productsList } = useQuery({
+  const { isLoading, data: productsList } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
       const { data } = await api.get(
         "/products?page=1&rows=8&sortBy=name&orderBy=ASC"
       );
-      return data.products
+      return data.products;
     },
   });
 
